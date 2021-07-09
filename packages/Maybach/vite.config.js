94a1db59ef271426f9eaf8f825e59471655caea7
Roadster <jsonkk@163.com>
2021-07-09 20:57:29 +0800

    feat: 测试package引入成功

---
+++ packages/Maybach/vite.config.js
@@ -0,0 +1,3 @@
+// This package is intended to be used only in client-side packages
+import { getClientConfiguration } from '../../config'
+export default getClientConfiguration()
--- platform-apps/city-app/src/main.ts
+++ platform-apps/city-app/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { testFunction } from "@maybach/components";
 
+testFunction();
 createApp(App).mount('#app')
